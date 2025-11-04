a63a45270170e0bd28b14d02544397da817e7702
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2025-11-03 22:28:04 -0500

    CBB patches for 5772 and 6752 are reproducible now

--- build.bat
+++ build.bat
@@ -12,13 +12,35 @@ mkdir output\patch_sections
 
 echo Building kernel patch files...
 
-bin\xenon-as.exe src\jasper_khv_vfuses.S -I src\include -o output\patch_sections\jasper_khv_vfuses.elf
-bin\xenon-objcopy.exe output\patch_sections\jasper_khv_vfuses.elf -O binary output\patch_sections\jasper_khv_vfuses.bin
-del output\patch_sections\jasper_khv_vfuses.elf
+bin\xenon-as.exe src\khv_17559_vfuses.S -I src\include -o output\patch_sections\khv_17559_vfuses.elf
+bin\xenon-objcopy.exe output\patch_sections\khv_17559_vfuses.elf -O binary output\patch_sections\khv_17559_vfuses.bin
+del output\patch_sections\khv_17559_vfuses.elf
 
-bin\xenon-as.exe src\jasper_khv_glitch2.S -I src\include -o output\patch_sections\jasper_khv_glitch2.elf
-bin\xenon-objcopy.exe output\patch_sections\jasper_khv_glitch2.elf -O binary output\patch_sections\jasper_khv_glitch2.bin
-del output\patch_sections\jasper_khv_glitch2.elf
+bin\xenon-as.exe src\khv_17559_glitch2.S -I src\include -o output\patch_sections\khv_17559_glitch2.elf
+bin\xenon-objcopy.exe output\patch_sections\khv_17559_glitch2.elf -O binary output\patch_sections\khv_17559_glitch2.bin
+del output\patch_sections\khv_17559_glitch2.elf
+
+echo Building 4BL patch files...
+
+
+
+echo Building 2BL patch files...
+
+bin\xenon-as.exe src\cbb_5772_vfuses.S -I src\include -o output\patch_sections\cbb_5772_vfuses.elf
+bin\xenon-objcopy.exe output\patch_sections\cbb_5772_vfuses.elf -O binary output\patch_sections\cbb_5772_vfuses.bin
+del output\patch_sections\cbb_5772_vfuses.elf
+
+bin\xenon-as.exe src\cbb_5772_glitch2.S -I src\include -o output\patch_sections\cbb_5772_glitch2.elf
+bin\xenon-objcopy.exe output\patch_sections\cbb_5772_glitch2.elf -O binary output\patch_sections\cbb_5772_glitch2.bin
+del output\patch_sections\cbb_5772_glitch2.elf
+
+bin\xenon-as.exe src\cbb_6752_vfuses.S -I src\include -o output\patch_sections\cbb_6752_vfuses.elf
+bin\xenon-objcopy.exe output\patch_sections\cbb_6752_vfuses.elf -O binary output\patch_sections\cbb_6752_vfuses.bin
+del output\patch_sections\cbb_6752_vfuses.elf
+
+bin\xenon-as.exe src\cbb_6752_glitch2.S -I src\include -o output\patch_sections\cbb_6752_glitch2.elf
+bin\xenon-objcopy.exe output\patch_sections\cbb_6752_glitch2.elf -O binary output\patch_sections\cbb_6752_glitch2.bin
+del output\patch_sections\cbb_6752_glitch2.elf
 
 echo.
 echo All Done!
